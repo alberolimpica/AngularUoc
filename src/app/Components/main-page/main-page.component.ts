3af77b02e2b17ac1b053f0a5f08a1b0a239bfeb2
Sara Alberola Mahiques <sara.alberola@outlook.es>
2021-03-14 08:50:51 +0100

    Main page fixes

--- src/app/Components/main-page/main-page.component.ts
+++ src/app/Components/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Activity } from 'src/app/Models/activity';
+import { ActivitiesService } from 'src/app/Services/activities.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  activities:Activity[] = [];
+  selectedActivity?: Activity;
+
+  constructor(private activitiesService: ActivitiesService) { }
 
   ngOnInit(): void {
+    this.getActivities();
+  }
+
+
+  getActivities(): void {
+    this.activitiesService.getActivities().subscribe(activities => this.activities = activities);
+  }
+
+  onSelect(activity: Activity): void {
+    this.selectedActivity = activity;
   }
 
 }
